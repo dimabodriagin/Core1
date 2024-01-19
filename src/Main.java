f96e1cd57152f57fdf89352528d244ff91b805b2
dimabodriagin <65782955+dimabodriagin@users.noreply.github.com>
2024-01-19 21:44:04 +0400

    Change check divide by zero

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,9 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1);
-        int c;
+        int c = calc.devide.apply(a, b); // Ошибка возникает из-за деления на ноль
+        calc.println.accept(c);
 
-
-        if (calc.notNull.test(b)) {
-            c = calc.devide.apply(a, b); // Ошибка возникает из-за деления на ноль
-            calc.println.accept(c);
-        }
 
         OnTaskDoneListener listener = System.out::println;
         OnTaskErrorListener errorListener = System.out::println;
